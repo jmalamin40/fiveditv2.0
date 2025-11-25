d47b3bf48d65ba4c4b50337ef015d4a6326153e7
Iman <iman@imanseattlesoftwaredevelopers.com>
2025-11-25 22:56:27 +0600

    deploying admin

--- next.config.js
+++ next.config.js
@@ -2,6 +2,21 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: false, // Disable SWC minify to avoid thread pool issues on shared hosting
+  
+  // Exclude admin folder from Next.js build
+  webpack: (config, { isServer }) => {
+    config.watchOptions = {
+      ...config.watchOptions,
+      ignored: ['**/admin/**', '**/node_modules/**'],
+    }
+    return config
+  },
+  
+  // Exclude admin from page extensions and other processing
+  pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
+  
+  // Exclude admin folder from static file serving
+  distDir: '.next',
 }
 
 module.exports = nextConfig
